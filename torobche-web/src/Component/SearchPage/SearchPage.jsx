308d3e244d08cd3719a0f16c38f63c3481998414
amirhossein <a.sabeti12@gmail.com>
2022-07-01 03:31:04 +0430

    compliting first Page

--- torobche-web/src/Component/SearchPage/SearchPage.jsx
+++ torobche-web/src/Component/SearchPage/SearchPage.jsx
@@ -6,6 +6,34 @@ const SearchPage = () =>{
         <div class="container">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <div class="SearchPageHeader">
+                <div class="signUp">
+                <button>ورود/ ثبت نام</button>
+                </div>
+            <div className='ddown'>
+            <div class="dropdown">
+                <button class="dropbtn">لپ تاپ
+                 </button>
+                 <div class="dropdown-content">
+                    <a href="#">لب تاپ لنوو</a>
+                    <a href="#">لب تاپ ایسوس</a>
+                    <a href="#">لب تاپ اپل</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">موبایل و تبلت 
+                 </button>
+                 <div class="dropdown-content">
+                    <a className='maincategory' href="#">گوشی موبایل</a>
+                    <a href="#">گوشی سامسونگ</a>
+                    <a href="#">گوشی شیایمی</a>
+                    <a href="#">گوشی اپل</a>
+                    <a className='maincategory' href="#">تبلت</a>
+                    <a href="#">تبلت سامسونگ</a>
+                    <a href="#">تبلت شیایمی</a>
+                    <a href="#">تبلت اپل</a>
+                </div>
+            </div>  
+            </div>
             </div>
             <div class="SearchPageBody">
                 <img src={TorobLogo} width="200px" height="100px"/>  
