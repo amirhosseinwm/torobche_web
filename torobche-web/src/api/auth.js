10bc8fc0920f876905f469b5c1f8c8a8db8a4162
Hasan Zonuzi <hasanzon@yahoo.com>
2022-07-02 20:52:43 +0430

    fix: fix api calls

--- torobche-web/src/api/auth.js
+++ torobche-web/src/api/auth.js
@@ -1,34 +1,34 @@
 import { baseUrl } from "./baseUrl";
 import axios from "axios";
 
-export const signUpApi = async (
-    username,
-    email,
-    password,
-) => {
+export const signUpApi = async (username, email, password) => {
     const url = `${baseUrl}/user/signup/v0/`;
     const response = await axios.post(url, {
         username,
         email,
         password,
     });
+    localStorage.setItem("token", response.data.token);
     return response.data;
-}
+};
 
-export const logInApi = async (
-    username,
-    password,
-) => {
+export const logInApi = async (username, password) => {
     const url = `${baseUrl}/user/login/v0/`;
     const response = await axios.post(url, {
         username,
         password,
     });
+    localStorage.setItem("token", response.data.token);
     return response.data;
-}
+};
 
 export const logOutApi = async () => {
+    const token = localStorage.getItem("token");
     const url = `${baseUrl}/user/signout/v0/`;
-    const response = await axios.post(url);
+    const response = await axios.post(url, {
+        headers: {
+            Authorization: `Token ${token}`,
+        },
+    });
     return response.data;
-}
+};
--- torobche-web/src/api/product.js
+++ torobche-web/src/api/product.js
@@ -7,27 +7,45 @@ export const getAllProductsApi = async (
     category,
     price_range
 ) => {
+    const token = localStorage.getItem("token");
     const url = `${baseUrl}/product/v0/`;
+    const response = await axios.get(
+        url,
+        {
+            params: {
+                ordering,
+                search,
+                category,
+                price_range,
+            },
+        },
+        {
+            headers: {
+                Authorization: `Token ${token}`,
+            },
+        }
+    );
+    return response.data;
+};
+
+export const getProductApi = async (product_id) => {
+    const token = localStorage.getItem("token");
+    const url = `${baseUrl}/product/${product_id}/v0/`;
     const response = await axios.get(url, {
-        params: {
-            ordering,
-            search,
-            category,
-            price_range,
+        headers: {
+            Authorization: `Token ${token}`,
         },
     });
     return response.data;
-}
-
-export const getProductApi = async (product_id) => {
-    const url = `${baseUrl}/product/${product_id}/v0/`;
-    const response = await axios.get(url);
-    return response.data;
-}
+};
 
 export const getCategoryApi = async () => {
+    const token = localStorage.getItem("token");
     const url = `${baseUrl}/product/categories/v0/`;
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+        headers: {
+            Authorization: `Token ${token}`,
+        },
+    });
     return response.data;
-}
-
+};
